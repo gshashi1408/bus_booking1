f13c1657d4fce92cc208527bf014ee12fd71beab
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-01-22 20:12:17 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,9 +1,5 @@
-
-def call(String name = 'value'){
-    echo "Building java project & ${name}"
-    withSonarQubeEnv('sonar') {
-    sh '''
-       mvn clean package sonar:sonar
-    '''
-}
-}
+def call() {
+       // Your SonarQube scan steps go here
+       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.170.23.153:9000 -Dsonar.java.binaries=target/classes'
+   }
+return this
